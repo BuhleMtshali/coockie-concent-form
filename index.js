a35396c8c79db70a8703d497a8bb030e28ecd25b
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-07-16 12:50:42 +0200

    updated the index file

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const modalElement = document.getElementById("modal-container");
+const modalElement = document.getElementById("outer-modal");
 const removeBtn = document.getElementById("close-btn");
 const formElement = document.getElementById("form");
 const textContainer = document.getElementById("text-container");
 const declineButton = document.getElementById("decline");
 const buttonContainer = document.getElementById("button-container");
+
+removeBtn.addEventListener("click", removeModal);
+function removeModal() {
+  modalElement.style.display = "none";
+}
+
+//function for reverse flex
+function changeButtons() {
+  buttonContainer.classList.toggle("reverse");
+}
+declineButton.addEventListener("mouseenter", changeButtons);
 
 //settimeout function for the modal to be displayed
 
@@ -11,16 +22,12 @@ setTimeout(function () {
   modalElement.style.display = "inline";
 }, 3000);
 
-//function to remove the modal
-
-function removeModal() {
-  modalElement.style.display = "none";
-}
-removeBtn.addEventListener("click", removeModal);
-
 //submit form function
-function formSubmitted(e) {
-  e.preventDefault();
+function formSubmitted(event) {
+  event.preventDefault();
+  const consentFormData = new FormData(formElement);
+  const fullName = consentFormData.get("fullName");
+
   textContainer.innerHTML = `
   <div class="modal-inner-loading">
         <i class="fa-solid fa-spinner fa-spin loading" style="color: #f10427;"></i>
@@ -35,23 +42,17 @@ function formSubmitted(e) {
   }, 6000);
 
   setTimeout(function () {
-    modalElement.innerHTML = `
-    <div class="close-btn-container">
-          <button id="close-btn">X</button>
-        </div>
-    <h2>Thanks you sucker!</h2>
+    const newContent = `
+        
+    <h2>Thanks ${fullName} you sucker!</h2>
     <img src="img/evil-laugh.jpg" class="meme"/>
     <p id="uploadText">
         We just sold the right to your eternal soul.
         </p>
     `;
-  }, 9000);
+    document.querySelector(".modal-container").innerHTML = newContent;
+    removeBtn.disabled = false;
+  }, 8000);
 }
 
 formElement.addEventListener("submit", formSubmitted);
-
-//function for reverse flex
-function changeButtons() {
-  buttonContainer.classList.toggle("reverse");
-}
-declineButton.addEventListener("mouseenter", changeButtons);
