097e65c54bde7feedd219e3314aeb00f409f1680
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-07-16 02:13:27 +0200

    updated the index file

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const modalElement = document.getElementById("modal-container");
+const removeBtn = document.getElementById("close-btn");
 
 setTimeout(function () {
   modalElement.style.display = "inline";
 }, 3000);
+
+removeBtn.addEventListener("click", removeModal);
+
+function removeModal() {
+  modalElement.style.display = "none";
+}
